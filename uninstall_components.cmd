63afb269f15f81201d4916fb7e6fe67a7398d65d
Martin Hertz <mvhertz@gmail.com>
2021-03-04 13:27:41 +0100

    update

--- uninstall_components.cmd
+++ uninstall_components.cmd
@@ -1,10 +1,6 @@
 cd "%~dp0"
 rd /s /q msys64
 rd /s /q msys64 2>nul
-rd /s /q gtk-cache
-rd /s /q gtk-cache 2>nul
-rd /s /q OpenSSL-Win64
-rd /s /q OpenSSL-Win64 2>nul
 rd /s /q nsis
 rd /s /q nsis 2>nul
 "%programfiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" --installPath "%~dp0msvc" /uninstall -q
