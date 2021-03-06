8715b864c5d0b3ed3539aa9b7bbda7f475f3a170
Martin Hertz <mvhertz@gmail.com>
2021-03-06 18:32:27 +0100

    update

--- install_components.cmd
+++ install_components.cmd
@@ -19,7 +19,7 @@ for /f %%i in ('curl https://strawberryperl.com/ ^| grep download/[0-9] ^| cut -
 mkdir perl
 curl -O https://strawberryperl.com/download/%var%/strawberry-perl-%var%-64bit.zip
 bsdtar xf strawberry-perl-%var%-64bit.zip --exclude="^c" -C perl
-curl.exe -kO https://download.visualstudio.microsoft.com/download/pr/68d6b204-9df0-4fcc-abcc-08ee0eff9cb2/0b833c703ae7532e54db2d1926e2c3d2e29a7c053358f8c635498ab25bb8c590/vs_BuildTools.exe
+curl -LO https://aka.ms/vs/16/release/vs_BuildTools.exe
 vs_buildtools.exe --quiet --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --installPath "%~dp0msvc" --wait
 del msys2-base-x86_64-latest.* NsProcess.zip strawberry-perl-*-64bit.zip vs_BuildTools.exe
 rd /s /q nsprocess
