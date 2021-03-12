4930611effd4d572c39b423e9be42a14d3e6099c
Martin Hertz <mvhertz@gmail.com>
2021-03-12 22:20:03 +0100

    update

--- install_components.cmd
+++ install_components.cmd
@@ -19,8 +19,8 @@ for /f %%i in ('curl https://strawberryperl.com/ ^| grep download/[0-9] ^| cut -
 mkdir perl
 curl -O https://strawberryperl.com/download/%var%/strawberry-perl-%var%-64bit.zip
 bsdtar xf strawberry-perl-%var%-64bit.zip --exclude="^c" -C perl
-if exist "%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" "%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version [16.0,17.0) -prerelease -products * -nologo | findstr . && set dirty=1
-if not defined dirty curl -LO https://aka.ms/vs/16/release/vs_BuildTools.exe && vs_BuildTools.exe --quiet --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --installPath "%~dp0msvc" --wait && del vs_BuildTools.exe
+if exist "%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" "%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version [16.0,17.0) -prerelease -products * -nologo -requires Microsoft.Component.MSBuild Microsoft.VisualStudio.Component.VC.Tools.x86.x64 | findstr . && set dirty=1
+if exist "%programfiles(x86)%\Windows kits\10" if not defined dirty curl -LO https://aka.ms/vs/16/release/vs_BuildTools.exe && vs_BuildTools.exe --quiet --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 --installPath "%~dp0msvc" --wait && del vs_BuildTools.exe
 del msys2-base-x86_64-latest.* NsProcess.zip strawberry-perl-*-64bit.zip
 rd /s /q nsprocess
 rd /s /q nsprocess 2>nul
